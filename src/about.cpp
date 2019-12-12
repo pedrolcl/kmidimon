5b1e70f5758674d5c4eae4c624e5eb308729bd05
Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
2019-12-12 13:40:23 +0000

    Modernization of the CMake build system Copyright year updated
    git-svn-id: https://svn.code.sf.net/p/kmidimon/code/trunk@208 77331063-5eac-44b6-ab16-8914078c1124

--- src/about.cpp
+++ src/about.cpp
@@ -1,6 +1,6 @@
 /*
     KMetronome - ALSA Sequencer based MIDI metronome
-    Copyright (C) 2006-2014, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+    Copyright (C) 2006-2019, Pedro Lopez-Cabanillas <plcl@users.sf.net>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
